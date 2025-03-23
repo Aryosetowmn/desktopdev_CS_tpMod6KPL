beb4c4cb20f63cb5dd0cf75ff768e1de53322d85
Aryosetowmn <aryosetowmnwork@gmail.com>
2025-03-23 23:51:28 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (string.IsNullOrEmpty(title))
+        {
+            throw new ArgumentException("Title cannot be null or empty");
+        }
+
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count < 0)
+        {
+            throw new ArgumentException("Play count cannot be negative");
+        }
+        playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"Video ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Aryoseto");
+        video.PrintVideoDetails();
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetails();
+    }
+}
